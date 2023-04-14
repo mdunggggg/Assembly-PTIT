6dfea2a16de98f796ad3ace3f3294c65cced3a3d
mdunggggg <dungmanh74@gmail.com>
2023-04-14 16:03:51 +0700

    Created

---
+++ Bai1.asm
@@ -0,0 +1,35 @@
+.Model Small
+.Stack 100H
+.Data
+    English db "Hello$"
+    VietNam db "Xin chao$"
+    crlf db 13, 10, '$' 
+.Code
+Main Proc    
+   
+    mov ax, @data
+    mov ds, ax
+    
+    mov ah, 9
+    lea dx, English
+    int 21h        
+    
+    lea dx, crlf
+    int 21h
+    
+    lea dx, VietNam
+    int 21h
+    
+    mov ah, 4ch
+    int 21h
+   
+    
+   
+Main endp 
+
+          
+            
+          
+          
+
+END MAIN
